10e078e3a26f930eac0b51fecbc3de68b307c4a0
erick-henri <erick.h.a@hotmail.com>
2020-11-03 16:50:03 -0300

    Alteração do ClienteVO

--- src/projeto/model/vo/ClienteVO.java
+++ src/projeto/model/vo/ClienteVO.java
@@ -6,4 +6,32 @@ public class ClienteVO {
 	private String endereço;
 	private String complemento;
 	
+	public String getNome(){ 
+		return nome;
+	}
+	
+	public void setNome(String nome){
+		
+	}
+	public String getCpf(){
+		return cpf;
+	}
+	public void setCpf( String cpf){
+		
+	}
+	public String getEndereço(){
+		return enredeço;
+	}
+	
+	public void setEndereço(String endereço){
+		
+		
+	}
+	public String getComplemento(){
+		return complemento;
+	}
+	public void setComplemento(String complemento){
+		
+		
+	}
 }  
