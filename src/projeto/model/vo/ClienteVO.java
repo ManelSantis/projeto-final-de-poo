2f7a6d3dbf6c3ed6e9fc88aabd03d085ca6b3c74
Manuel Maria dos Santos Neto <manelsantis04@gmail.com>
2020-11-04 15:38:04 -0300

    Alterações nos métodos de ClienteVO

--- src/projeto/model/vo/ClienteVO.java
+++ src/projeto/model/vo/ClienteVO.java
@@ -1,41 +1,56 @@
 package projeto.model.vo;
-	
+
 public class ClienteVO {
 	private String nome;
 	private String cpf;
-	private String endereço;
+	private String endereco;
 	private String complemento;
-	
-	public String getNome(){ 
-		return nome;
 
+	public String getNome() {
+		return nome;
 	}
-	
-	public void setNome(String nome){
-		this.nome = nome;
+
+	public void setNome(String nome) {
+		if ((nome.length() <= 100) && (!nome.isEmpty())) {
+			this.nome = nome; // limitar o tamanho do nome do cliente e se está vazio
+		} else {
+			System.out.print("Operação invalida");
+		}
 	}
-	public String getCpf(){
+
+	public String getCpf() {
 		return cpf;
 	}
-	public void setCpf( String cpf){
-		if (cpf != 12){
-			System.out.println("CPF está invalido.")
+
+	public void setCpf(String cpf){
+		if ((cpf.length() != 11) && (!cpf.isEmpty())){
+			System.out.println("CPF está invalido."); //se o tamnho está certo, e se está vazio
+		} else {
+			this.cpf = cpf;
 		}
-		else this.cpf = cpf;
 	}
-	public String getEndereço(){
-		return enredeço;
+
+	public String getEndereco() {
+		return endereco;
 	}
-	
-	public void setEndereço(String endereço){
-		
-		
+
+	public void setEndereço(String estado, String cidade, String bairro, String rua, String numero) {
+		if ((!estado.isEmpty()) && (!cidade.isEmpty()) && (!bairro.isEmpty()) && (!rua.isEmpty()) && (!numero.isEmpty())) {
+			this.endereco = estado + "\n" + cidade + "\n" + bairro + "\n" + rua + "\n" + numero; //salvar tudo separado por \n para ficar mais facil de verificar depois
+		} else {
+			System.out.println("Não foi possivel salvar o seu endereço!");
+		}
 	}
-	public String getComplemento(){
+
+	public String getComplemento() {
 		return complemento;
 	}
-	public void setComplemento(String complemento){
-		String nome = complemento;
-		
+
+	public void setComplemento(String complemento) {
+		if ((complemento.length() <= 100) && (!complemento.isEmpty())){
+			this.complemento = complemento; // limitar o tamanho do complemento e verificar se não está vazio
+		} else {
+			System.out.print("Operação inválida");
+		}
 	}
-}  
+}
