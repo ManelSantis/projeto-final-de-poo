d2f3e65eed0aeb49226638070ea3319b17ddca0c
Manuel Maria dos Santos Neto <manelsantis04@gmail.com>
2020-11-04 15:43:47 -0300

    Alterações em alguns métodos set do arquivo ProdutoVO

--- src/projeto/model/vo/ProdutoVO.java
+++ src/projeto/model/vo/ProdutoVO.java
@@ -3,20 +3,20 @@ package projeto.model.vo;
 public class ProdutoVO {
 	private String nome;
 	private String descricao;
-	private double preco; 
-	private String serie; //número de serie geralmente tem 13 caracteres
-	private int quantidade; //quantKanalence + quantToinho
-	private String img; //um endereço string
-	
+	private double preco;
+	private String serie; // número de serie geralmente tem 13 caracteres
+	private int quantidade; // quantKanalence + quantToinho
+	private String img; // um endereço string
+
 	public String getNome() {
 		return nome;
 	}
-	
+
 	public void setNome(String nome) {
-		if (nome.length() <= 100) {
-			this.nome = nome; //limitar o tamanho do nome do produto
+		if ((nome.length() <= 100) && (!nome.isEmpty())) {
+			this.nome = nome; // limitar o tamanho do nome do produto e se não está vazio
 		} else {
-			System.out.print("Ultrapassou o limite de caracteres!");
+			System.out.print("Operação inválida!");
 		}
 	}
 
@@ -25,10 +25,10 @@ public class ProdutoVO {
 	}
 
 	public void setDescricao(String descricao) {
-		if (descricao.length() <= 500) {
-			this.descricao = descricao; //limitar o tamanho da descrição do produto
+		if ((descricao.length() <= 500) && (!descricao.isEmpty())) {
+			this.descricao = descricao; //limitar o tamanho da descrição do produto e se não está vazia
 		} else {
-			System.out.print("Ultrapassou o limite de caracteres!");
+			System.out.print("Operação inválida!");
 		}
 	}
 
@@ -38,7 +38,7 @@ public class ProdutoVO {
 
 	public void setPreco(double preco) {
 		if (preco > 0) {
-			this.preco = preco; //verificar se o preço está sendo um valor positivo e maior que zero
+			this.preco = preco; // verificar se o preço está sendo um valor positivo e maior que zero
 		} else {
 			System.out.print("Quantidade inválida!");
 		}
@@ -50,7 +50,7 @@ public class ProdutoVO {
 
 	public void setSerie(String serie) {
 		if (serie.length() == 13) {
-		this.serie = serie; //verificar se o tamanho do numero de serie está correto
+			this.serie = serie; // verificar se o tamanho do numero de serie está correto
 		} else {
 			System.out.println("Digite novamente!");
 		}
@@ -60,14 +60,14 @@ public class ProdutoVO {
 		return quantidade;
 	}
 
-	public void setQuantidade(int quantKanalence, int quantToinho) {
-		if(quantKanalence < 0) {
+	public void setQuantidade(int quantKanalence, int quantToinho) { // pegar as duas quantidades e depois soma-las
+		if (quantKanalence < 0) {
 			System.out.println("Quantidade inválida!");
 		}
-		if(quantToinho < 0) {
+		if (quantToinho < 0) {
 			System.out.println("Quantidade inválida!");
 		}
-		if((quantKanalence + quantToinho) > 0) {
+		if ((quantKanalence + quantToinho) > 0) {
 			this.quantidade = quantKanalence + quantToinho;
 		} else {
 			System.out.println("Quantidade total inválida!");
@@ -79,6 +79,10 @@ public class ProdutoVO {
 	}
 
 	public void setImg(String img) {
-		this.img = img; //não acho que precise de um ecessão, além do link ter que ser valido
+		if (!img.isEmpty()) {
+			this.img = img; // se não estiver vazia, salva
+		} else {
+			System.out.println("Adicione uma imagem!");
+		}
 	}
 }
