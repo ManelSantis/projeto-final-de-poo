a475e936008d2518ebad643912a1d382f614c3c8
Manuel Maria dos Santos Neto <manelsantis04@gmail.com>
2020-11-04 23:11:52 -0300

    Correção de um erro no arquivo VendaVO

--- src/projeto/model/vo/VendaVO.java
+++ src/projeto/model/vo/VendaVO.java
@@ -7,48 +7,55 @@ public class VendaVO {
 	private ProdutoVO produto;
 	private int quantidade;
 	private Calendar data;
-	
-	public ClienteVO getCliente(){
+
+	public ClienteVO getCliente() {
 		return cliente;
 	}
-	public void  setCliente(ClienteVO cliente){
-	      if (cliente != null){
+
+	public void setCliente(ClienteVO cliente) {
+		if (cliente != null) {
 			this.cliente = cliente; // Atribui os valores ao metodo da classe cliente.
-	} else {
-	      System.out.println("Operação inválida");
-	}
+		} else {
+			System.out.println("Operação inválida");
 		}
-	public ProdutoVO getProduto(){
+	}
+
+	public ProdutoVO getProduto() {
 		return produto;
 	}
-	public void  setProduto(ProdutoVO produto){
-	      if (produto != null){
+
+	public void setProduto(ProdutoVO produto) {
+		if (produto != null) {
 			this.produto = produto; // Atribui os valores ao metodo da classe produto.
-	} else {
-	      System.out.println("Operação inválida");
-	}
+		} else {
+			System.out.println("Operação inválida");
 		}
-	public int getQuantidade(){
+	}
+
+	public int getQuantidade() {
 		return quantidade;
 	}
-	public void setQuantidade(int quantidade){
-		if (quantidade <= 0){ // Se a quantidade inseria for menor que 0 ele vai informar que está invalido, se não irá armazenar o valor.
-			System.out.print("Quantidade de produtos invalida")
-		}
-		else this.quantidade =  quantidade;
+
+	public void setQuantidade(int quantidade) {
+		if (quantidade <= 0) { // Se a quantidade inseria for menor que 0 ele vai informar que está invalido, se não irá armazenar o valor.
+			System.out.print("Quantidade de produtos invalida");
+		} else
+			this.quantidade = quantidade;
 	}
-	public Calendar getData(){
+
+	public Calendar getData() {
 		return data;
 	}
-	public void setData(){
+
+	public void setData() {
 		// Utilizando a classe calendar para gerar todas as informações referente a data da compra.
-		Calendar data = Calendar.getInstance(); 
+		Calendar data = Calendar.getInstance();
 		int ano = data.get(Calendar.YEAR);
 		int mes = data.get(Calendar.MONTH);
 		int dia = data.get(Calendar.DAY_OF_MONTH);
 		int hora = data.get(Calendar.HOUR_OF_DAY);
 		int minutos = data.get(Calendar.MINUTE);
 		int segundos = data.get(Calendar.SECOND);
-                this.data = data;
+		this.data = data;
 	}
 }
