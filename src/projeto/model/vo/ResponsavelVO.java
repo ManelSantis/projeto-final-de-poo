55fc54f870ae82e9df4ff2ed66d2df95bb7c3bc0
Manuel Maria dos Santos Neto <manelsantis04@gmail.com>
2020-11-12 18:18:05 -0300

    Métodos get e set do arquivo ResponsavelVO

--- src/projeto/model/vo/ResponsavelVO.java
+++ src/projeto/model/vo/ResponsavelVO.java
@@ -1,5 +1,40 @@
 package projeto.model.vo;
 
-public class ResponsavelVO extends PessoaVO{
+public class ResponsavelVO extends PessoaVO {
+	private String usuario;
+	private String senha;
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		if (usuario != null) {
+			if (!usuario.isEmpty()) {
+				this.usuario = usuario;
+			} else {
+				System.out.println("Inválido");
+			}
+		} else {
+			System.out.println("Inválido");
+		}
+
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		if (senha != null) {
+			if (!senha.isEmpty()) {
+				this.senha = senha;
+			} else {
+				System.out.println("Inválido");
+			}
+		} else {
+			System.out.println("Inválido");
+		}
+	}
 
 }
