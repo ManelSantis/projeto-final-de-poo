1be660b8fe931f9e45882acbcaa15e986b41247c
Manuel Maria dos Santos Neto <manelsantis04@gmail.com>
2020-11-12 20:20:22 -0300

    Começo dos métodos de ClienteBO e LocalBO e adição de novo arquivo ResponsavelBO

--- src/projeto/model/bo/ClienteBO.java
+++ src/projeto/model/bo/ClienteBO.java
@@ -1,20 +1,45 @@
-package projeto.mode.bo;
+package projeto.model.bo;
+
+import projeto.model.vo.ClienteVO;
+import projeto.model.vo.VendaVO;
 
 public class ClienteBO {
-	void cadastrar() {
-
+	public void cadastrar(String nome, String cpf, String telefone, String estado, String cidade, String bairro, String rua, String numero) {
+		//esse metodo irá receber todos os dados que são salvos em um clienteVO a aprtir de parametros
+		//então será criado um objeto clientevo, para poder salvar os dados
+		//dentro do banco de dados
+		ClienteVO c = new ClienteVO();
+		c.setCpf(cpf);
+		c.setEndereco(estado, cidade, bairro, rua, numero);
+		c.setNome(nome);
+		c.setTelefone(telefone);
+		//então será salvado no banco de dados
 	}
 
-	void editar() {
-
+	public void editar(String id) {
+		//será pesquisado no bd o cliente que tenha o id indicado para editar
+		//o que está sendo salvo está assim apenas como demonstração, 
+		//depois será alterado
+		ClienteVO c = new ClienteVO();
+		c.setCpf("cpf");
+		c.setEndereco("estado", "cidade", "bairro", "rua", "numero");
+		c.setNome("nome");
+		c.setTelefone("telefone");
+		//e então salvo no banco de dados
 	}
 
-	void excluir() {
-
+	public void excluir(ClienteVO c) {
+		String id = c.getCpf();
+		//será pesquisado e então deletado no bd a partir do id do cliente apontado
 	}
 
-	void historicoDeCompras() {
-
+	public VendaVO[] historicoDeCompras(String id) {
+		//nesse metodo terá que ser primeiro feita uma consulta no banco de dados para saber
+		//quantas vendas foram feitas com o id (no caso o cpf) do cliente
+		int quantVendas =  0;
+		//então elas serão guardadas em um array para depois serem listadas
+		VendaVO vendas[] = new VendaVO[quantVendas];
+		return vendas;
 	}
 
 }
--- src/projeto/model/bo/LocalBO.java
+++ src/projeto/model/bo/LocalBO.java
@@ -1,19 +1,31 @@
 package projeto.model.bo;
 
+import projeto.model.vo.LocalVO;
+import projeto.model.vo.ProdutoVO;
+
 public class LocalBO {
-	void cadastrar() {
-
+	public void cadastrar(String sala, String estante, String compartimento) {
+		//serão pegos os parâmetros dos metodos para que eles possão então ser utilizados com LocalVO
+		LocalVO l = new LocalVO();
+		l.setLocalizacao(sala, estante);
+		l.setNumCompartimento(compartimento);
+		//e então salvos no banco de dados
 	}
 
-	void adcionar() {
-
+	public void adicionar(LocalVO l) {
+		//esse método ligará o produto com um local, se o local existir
+		ProdutoVO p = new ProdutoVO();
+		p.setLocal(l);
+		//para assim poder salvar que tal produto esta em tal local
 	}
 
-	void excluir() {
-
+	public void excluir(LocalVO l) {
+		//irá pesquisar no banco de dados a partir do id para verificar se existe
+		String c = l.getNumCompartimento();
+		//salvará em uma variavel auxiliar para então poder deletar do banco de dados
 	}
 
-	void editar() {
-
+	public void editar(LocalVO l) {
+		l.setLocalizacao("nova sala", "nova estante");
 	}
 }
