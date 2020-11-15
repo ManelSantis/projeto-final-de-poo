28d39ee0a61a57b113fbe38c8e3237e6c2659bf6
Manuel Maria dos Santos Neto <manelsantis04@gmail.com>
2020-11-15 00:36:49 -0300

    Pequena edição em um dos métodos e organização dos comentarios

--- src/projeto/model/bo/ClienteBO.java
+++ src/projeto/model/bo/ClienteBO.java
@@ -5,9 +5,10 @@ import projeto.model.vo.VendaVO;
 
 public class ClienteBO {
 	public void cadastrar(String nome, String cpf, String telefone, String estado, String cidade, String bairro, String rua, String numero) {
-		//esse metodo irá receber todos os dados que são salvos em um clienteVO a aprtir de parametros
-		//então será criado um objeto clientevo, para poder salvar os dados
-		//dentro do banco de dados
+		// Esse metodo irá receber todos os dados que são salvos em um clienteVO
+		// a aprtir de parametros
+		// então será criado um objeto clientevo, para poder salvar os dados
+		// dentro do banco de dados
 		ClienteVO c = new ClienteVO();
 		c.setCpf(cpf);
 		c.setEndereco(estado, cidade, bairro, rua, numero);
@@ -17,27 +18,28 @@ public class ClienteBO {
 	}
 
 	public void editar(String id) {
-		//será pesquisado no bd o cliente que tenha o id indicado para editar
-		//o que está sendo salvo está assim apenas como demonstração, 
-		//depois será alterado
+		// Será pesquisado no bd o cliente que tenha o id indicado para editar
+		// o que está sendo salvo está assim apenas como demonstração, 
+		// depois será alterado
 		ClienteVO c = new ClienteVO();
 		c.setCpf("cpf");
 		c.setEndereco("estado", "cidade", "bairro", "rua", "numero");
 		c.setNome("nome");
 		c.setTelefone("telefone");
-		//e então salvo no banco de dados
+		// e então salvo no banco de dados
 	}
 
-	public void excluir(ClienteVO c) {
-		String id = c.getCpf();
-		//será pesquisado e então deletado no bd a partir do id do cliente apontado
+	public void excluir(String id) {
+		// Será pesquisado no banco de dados o cpf do cliente
+		// para então deleta-lo
 	}
 
-	public VendaVO[] historicoDeCompras(String id) {
-		//nesse metodo terá que ser primeiro feita uma consulta no banco de dados para saber
-		//quantas vendas foram feitas com o id (no caso o cpf) do cliente
+	public VendaVO[] historicoDeCompras(ClienteVO cli) {
+		// Nesse metodo terá que ser primeiro feita uma consulta 
+		// no banco de dados para saber
+		// quantas vendas foram feitas com o id (no caso o cpf) do cliente
 		int quantVendas =  0;
-		//então elas serão guardadas em um array para depois serem listadas
+		// então elas serão guardadas em um array para depois serem listadas
 		VendaVO vendas[] = new VendaVO[quantVendas];
 		return vendas;
 	}
