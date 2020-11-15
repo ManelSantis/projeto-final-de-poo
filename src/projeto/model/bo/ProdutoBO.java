de4c5275e7e05256d4b3f4427d60806e2c64b73d
Manuel Maria dos Santos Neto <manelsantis04@gmail.com>
2020-11-15 00:38:33 -0300

    Corrigindo alguns parametros dos metodos

--- src/projeto/model/bo/ProdutoBO.java
+++ src/projeto/model/bo/ProdutoBO.java
@@ -1,10 +1,12 @@
 package projeto.model.bo;
 
+import projeto.model.vo.LocalVO;
 import projeto.model.vo.ProdutoVO;
+import projeto.model.vo.ResponsavelVO;
 
 public class ProdutoBO {
 	public void cadastrar(String nome, String descricao, float peso,
-			String serie, double preco, int quantidade,
+			String serie, double preco, int quantK, int quantT,
 			ResponsavelVO responsavel, LocalVO local, String img) {
 		// Irá receber os dados dos parametros
 		// Repassando para um objeto produto e então encaminhar para ser salvo
@@ -14,37 +16,34 @@ public class ProdutoBO {
 		pro.setDescricao(descricao);
 		pro.setPeso(peso);
 		pro.setSerie(serie);
-		pro.setQuantidade(quantidade);
+		pro.setQuantidade(quantK, quantT);
 		pro.setResponsavel(responsavel);
 		pro.setLocal(local);
 		pro.setImg(img);
 	}
 
-	public void pesquisar(ProdutoVO produto) {
+	public ProdutoVO[] pesquisar(String pesquisa) {
 		// Irá identificar o produto no banco de dados a partir de qual atributo
-		// o usuário selecionou.
+		// o usuário selecionou, ou a partir do quão parecido é a String, e irá
+		// adiciona-los em um vetor de ProdutoVO para então ser mostrado
+		int quantProd = 0;
+		ProdutoVO produtos[] = new ProdutoVO[quantProd];
+		return produtos;
 	}
 
-	public void excluir(ProtudoVO proex) {
+	public void excluir(String serie) {
 		// Irá localizar no banco de dados o produto a partir do número de série
 		// do mesmo para realizar a exclusão.
-		String ex = proex.getSerie();
-
 	}
 
-	public void editar (ProdutoVO proed) {
-		
-		// Irá localizar o produto no banco de dados para realizar a edição.
-		// Sendo  passado a alteração para um objeto criado, para então salvar a informação
-		// no local selecionado.
-		ProdutoVO ed = new ProdutoVO;
-		se.setNome("nome");
-		se.setDescricao("descricao");
-		se.setPeso("peso");
-		se.setSerie("serie");
-		se.setQuantidade("quantidade");
-		se.setResponsavel("responsavel");
-		se.setLocal("local");
-		se.setImg("img");
+	public void editar (String serie) {
+		// Irá localizar o produto no banco de dados a partir do
+		// número de serie para realizar a edição.
+		// Sendo passado os dados encontrados para um objeto
+		// e então esses podendo ser alterados para
+		// ser realizado o update no banco de dados
+		ProdutoVO update = new ProdutoVO();
+
 	}
+	
 }
--- src/projeto/model/bo/ResponsavelBO.java
+++ src/projeto/model/bo/ResponsavelBO.java
@@ -1,21 +1,45 @@
 package projeto.model.bo;
 
 import projeto.model.vo.ResponsavelVO;
+import projeto.model.vo.VendaVO;
 import projeto.model.vo.ProdutoVO;
+
 public class ResponsavelBO {
-	public void estoque (ResponsavelVO reses){
-	// Irá exibir a quantidade do produto que tem no estoque do responsável
+	public ProdutoVO[] estoque(ResponsavelVO resp) {
+		// Irá procurar no banco de dados todos os produtos
+		// que tiverem o responsavel igual ao do parametro
+		// serão passados para um array para então serem
+		// mostrados
+		int quantProd = 0;
+		ProdutoVO produtos[] = new ProdutoVO[quantProd];
+		return produtos;
 	}
-	public void pesquisar(ProdutoVO propes){
-	// Irá pesquisar o produto que está na posse do responsável.
+
+	public ProdutoVO[] pesquisar(String pesquisa, ResponsavelVO resp) {
+		// Irá chamar o médodo estoque para que seja pesquisado apenas os produtos
+		// do responsavel que esteja logado
+		ProdutoVO esto[] = estoque(resp);
+		// e então será feita uma busca para saber quais produtos tem algo
+		// parecido com a String parametrizada, para serem postos em outro
+		// vetor, que exibirá os dados
+		int quantProd = 0;
+		ProdutoVO produtos[] = new ProdutoVO[quantProd];
+		return produtos;
 	}
-	public void HistoricoDeVendas(){
-	// Irá exibir a quantidade de produtos vendidos daquele responsável.
+
+	public VendaVO[] HistoricoDeVendas(ResponsavelVO resp) {
+		// Irá exibir as vendas que foram feitas com o responsavel
+		// indicado no parametro, para então serem postas em um
+		// vetor e então serem exibidas
+		int quantVen = 0;
+		VendaVO vendas[] = new VendaVO[quantVen];
+		return vendas;
 	}
-	public void Login(String usuario, String senha){
-	// Irá receber o nome de login e a senha e conferir se bate com o cadastrado.
-	// Caso coincidir, irá permitir o acesso.
-		
+
+	public void Login(String usuario, String senha) {
+		// Será receber o nome de login e a senha
+		// e conferir se bate com o cadastrado.
+		// Caso coincidir, irá permitir o acesso.
 	}
-	
+
 }
