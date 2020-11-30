9870aa26fd811573e3eade548635067dad655887
erick-henri <erick.h.a@hotmail.com>
2020-11-30 16:08:08 -0300

    Adição do código na classe LocalDAO e ProdutoDAO

--- src/projeto/model/dao/LocalDAO.java
+++ src/projeto/model/dao/LocalDAO.java
@@ -1,5 +1,59 @@
 package projeto.model.dao;
 
-public class LocalDAO {
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
+import projeto.model.vo.LocalVO;
+import projeto.model.vo.PessoaVO;
+import projeto.model.vo.VendaVO;
+
+public class LocalDAO extends BaseDao{
+	public void  cadastrar (LocalVO loca){
+		conn = getConnection();
+		String sql =  "insert into pessoa (localizacao,compartimento,responsavel) values (?,?,?)";
+		PreparedStatement ptst;
+		try {
+			ptst = conn.prepareStatement(sql);
+			ptst.setString(1, loca.getLocalizacao());
+			ptst.setString(2, loca.getCompartimento());
+			((Responsavel) ptst).setResponsavel(3, loca.getResponsavel());
+			ptst.execute();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+	public void remover (LocalVO loca){
+		conn = getConnection();
+		String sql =  "select from local where localizacao = ?";
+		PreparedStatement ptst;
+		try {
+			ptst = conn.prepareStatement(sql);
+			ptst.setString(1, loca.getLocalizacao());
+			ptst.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void editar(LocalVO loca) {
+		conn = getConnection();
+		String sql =  "update from local where localizacao = ?";
+		PreparedStatement ptst;
+		try {
+			ptst = conn.prepareStatement(sql);
+			ptst.setString(1, loca.getLocalizacao());
+			ptst.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
--- src/projeto/model/dao/ProdutoDAO.java
+++ src/projeto/model/dao/ProdutoDAO.java
@@ -1,11 +1,58 @@
 package projeto.model.dao;
 
-public class ProdutoDAO {
-	private String nome;
-	private String descricao;
-	private double preco;
-	private int serie;
-	private int quantidadeKanalence;
-	private int quantidadeToinho;
-	private String img;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import projeto.model.vo.ProdutoVO;
+
+public class ProdutoDAO extends BaseDao {
+	public void cadastrar(ProdutoVO prod) {
+		conn = getConnection();
+		String sql = "insert into produto ( nome,descricao,peso,serie,preco,quantidade,quantipedido,img) values (?,?,?,?,?,?,?,?)";
+		PreparedStatement ptst;
+		try {
+			ptst = conn.prepareStatement(sql);
+			ptst.setString(1, prod.getNome());
+			ptst.setString(2, prod.getDescricao());
+			ptst.setDouble(3, prod.getPeso());
+			ptst.setString(4, prod.getSerie());
+			ptst.setDouble(5, prod.getPreco());
+			ptst.setInt(6, prod.getQuantidade());
+			ptst.setInt(7, prod.getQuantiPedido());
+			ptst.setString(8, prod.getImg());
+			ptst.execute();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void excluir(ProdutoVO prod) {
+		conn = getConnection();
+		String sql = "delete from produto where cpf = ?";
+		PreparedStatement ptst;
+		try {
+			ptst = conn.prepareStatement(sql);
+			ptst.setString(1, prod.getSerie());
+			ptst.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void editar(ProdutoVO prod) {
+		conn = getConnection();
+		String sql =  "update from produto where serie = ?";
+		PreparedStatement ptst;
+		try {
+			ptst = conn.prepareStatement(sql);
+			ptst.setString(1, prod.getSerie());
+			ptst.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
