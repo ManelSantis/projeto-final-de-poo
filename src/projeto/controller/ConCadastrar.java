108e75b14e1b86de9b508855811cce7e4c6b2dd4
Manuel Maria dos Santos Neto <manelsantis04@gmail.com>
2020-12-07 00:17:54 -0300

    Edição no controller

--- src/projeto/controller/ConCadastrar.java
+++ src/projeto/controller/ConCadastrar.java
@@ -1,20 +1,44 @@
 package projeto.controller;
 
-import javafx.scene.input.MouseEvent;//como os botões do menu são labels, tem que usar o MouseEvent
+import javafx.event.ActionEvent;
 import projeto.view.Telas;
 
 public class ConCadastrar extends ConMenu{
-	public void inicio (MouseEvent e) throws Exception{
-		Telas.telaInicio();
-	}
-	public void cliente (MouseEvent e) throws Exception{
+	
+	
+	public void cliente (ActionEvent e) throws Exception{
 		Telas.telaClienteInicio();
 	}
-	public void responsavel (MouseEvent e) throws Exception{
+	
+	public void responsavel (ActionEvent e) throws Exception{
 		Telas.telaResponsavelInicio();
 	}
 	
-	public void sair (MouseEvent e) throws Exception{
-		Telas.telaLogin();
+	public void pesquisa (ActionEvent e) {
+		
+	}
+	
+	public void cadastroResponsavel(ActionEvent e) throws Exception {
+		Telas.telaResponsavelCadastrar();
+	}
+	
+	public void editarResponsavel(ActionEvent e) throws Exception {
+		Telas.telaResponsavelEditar();
+	}
+
+	public void excluirResponsavel(ActionEvent e) throws Exception {
+		Telas.telaResponsavelExcluir();
+	}
+	
+	public void cadastroCliente(ActionEvent e) throws Exception {
+		Telas.telaClienteCadastrar();
+	}
+	
+	public void editarCliente(ActionEvent e) throws Exception {
+		Telas.telaClienteEditar();
+	}
+
+	public void excluirCliente(ActionEvent e) throws Exception {
+		Telas.telaClienteExcluir();
 	}
 }
--- src/projeto/controller/ConEstoque.java
+++ src/projeto/controller/ConEstoque.java
@@ -11,7 +11,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
-import javafx.scene.input.MouseEvent;
 import projeto.view.Telas;
 
 public class ConEstoque extends ConMenu implements Initializable{
